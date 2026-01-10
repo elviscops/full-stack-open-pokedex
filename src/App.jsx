704f43b0a8fa1ac39ba9a30cb3d233efb2ef235a
Elvis Cops <elvis.cops93@gmail.com>
2026-01-10 16:02:35 +0100

    Exercise 11.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, useMatch } from 'react-router-dom'
+import React from 'react'
+import { Routes, Route, useMatch } from 'react-router-dom'
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
@@ -26,20 +27,21 @@ const App = () => {
   let previous = null
 
   if (match && match.params) {
-    const pokemonId = pokemonList.find(({ name }) => name === match.params.name).id
-    previous = pokemonList.find(({ id }) => id === pokemonId - 1)
-    next = pokemonList.find(({ id }) => id === pokemonId + 1)
+    // use index-based neighbors to align with rendered list order
+    const currentIndex = pokemonList.findIndex(({ name }) => name === match.params.name)
+    if (currentIndex !== -1) {
+      previous = pokemonList[currentIndex - 1] || null
+      next = pokemonList[currentIndex + 1] || null
+    }
   }
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PokemonList pokemonList={pokemonList} />} />
-        <Route path="/pokemon/:name" element={
-          <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
-        } />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<PokemonList pokemonList={pokemonList} />} />
+      <Route path="/pokemon/:name" element={
+        <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
+      } />
+    </Routes>
   )
 }
 
