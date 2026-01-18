8d830ae291dbfa4fb7c3580472e113b167c91ecd
Elvis Cops <elvis.cops93@gmail.com>
2026-01-18 12:09:40 +0100

    additional test cases

--- test/e2etests.spec.js
+++ test/e2etests.spec.js
@@ -3,7 +3,17 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
     await page.goto('/') 
-    await expect(page.getByText('Ivysaur')).toBeVisible()
+    await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+   test('pokemon page can be opened', async ({ page }) => {
+    await page.goto('/pokemon/ivysaur') 
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('speed')).toBeVisible()
+    await expect(page.getByText('special defense')).toBeVisible()
+    await expect(page.getByText('special attack')).toBeVisible()
+    await expect(page.getByRole('cell', { name: 'defense', exact: true })).toBeVisible()
+    await expect(page.getByRole('cell', { name: 'attack', exact: true })).toBeVisible()
+    await expect(page.getByText('hp')).toBeVisible()
   })
 })
